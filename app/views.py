369489463355fcec63eb440a8e359454319b6934
Diandra Whittick <diandra.whittick@mymona.uwi.edu>
2019-03-03 02:33:12 +0000

    Exercise 9

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -30,13 +31,24 @@ def about():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+   
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
+        
+    
         if form.username.data:
             # Get the username and password values from the form.
+             username= form.username.data
+             password =  form.password.data
+             user = UserProfile.query.filter_by(username=username).first()
+             if user is not None and check_password_hash(user.password,password):
+                 remember_me = False
+                 if'remeber_me' in request.form:
+                    remember_me = True
 
+                    
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
@@ -44,10 +56,10 @@ def login():
             # passed to the login_user() method below.
 
             # get user id, load into session
-            login_user(user)
-
+                 login_user(user,remember=remember_me)
+                 flash('Logged in Successfully','success')
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+                 return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
